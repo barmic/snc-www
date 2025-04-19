923668ba80a8d9e423dd538a124a405bc7dff1ee
Michel Barret <michel.barret@gmail.com>
2025-04-19 14:59:54 +0200

    travail en cours

---
+++ src/utils.ts
@@ -0,0 +1,12 @@
+import { DateTime } from "luxon";
+
+export default function parseDate(input: string): Date {
+  const match = input.match(/^(.+)\[(.+)\]$/);
+  if (match) {
+    const dateStr = match[1];
+    const zone = match[2];
+    const dt = DateTime.fromISO(dateStr, { zone });
+    return new Date(dt.toString()); // Affiche la date avec la zone correcte
+  }
+  return new Date(input);
+}
