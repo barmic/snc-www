24c5102c017d44a9aec0da6e9e2a29ef839e3ca4
Michel Barret <michel.barret@gmail.com>
2025-04-24 19:15:22 +0200

    travail en cours

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,9 @@
 import { DateTime } from "luxon";
 
-export default function parseDate(input: string): Date {
-  const match = RegExp(/^(.+)\[(.+)\]$/).exec(input);
+type translation = {fr: string, en: string};
+type language = keyof translation;
+function parseDate(input: string): Date {
+  const match = RegExp(/^(.+)\[(.+)]$/).exec(input);
   if (match) {
     const dateStr = match[1];
     const zone = match[2];
@@ -10,3 +12,14 @@ export default function parseDate(input: string): Date {
   }
   return new Date(input);
 }
+
+function url(path: string[] | string, opts: {anchor?: string, lang?: language} = {}) {
+  const lang = opts.lang ?? 'fr';
+  const langPart = lang === 'en' ? ['en'] : [];
+  const pathArray = typeof path === 'string' ? path.split('/') : path;
+  const a = [langPart , pathArray].flatMap((p) => p);
+  const result = `${import.meta.env.BASE_URL}/${a.join('/')}/${opts.anchor ?? ''}`.replaceAll(/\/+/g, '/');
+  return result.length === 1 || !result.endsWith('/') ? result : result.slice(0, -1);
+}
+
+export default {parseDate, url}
